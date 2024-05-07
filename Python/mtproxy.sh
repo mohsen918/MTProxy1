a939bc11535d85f616068b40fbfd72b7aaeae307
elesssss <162124503+elesssss@users.noreply.github.com>
2024-05-07 08:32:43 +0800

    Update mtproxy.sh

--- Python/mtproxy.sh
+++ Python/mtproxy.sh
@@ -79,39 +79,47 @@ check_pmc(){
     if [[ "$release" == "debian" || "$release" == "ubuntu" || "$release" == "kali" ]]; then
         updates="apt update -y"
         installs="apt install -y"
-        apps=("python3" "xxd")
+        check_install="dpkg -s"
+        apps=("python3" "python3-cryptography" "xxd")
     elif [[ "$release" == "almalinux" || "$release" == "fedora" || "$release" == "rocky" ]]; then
         updates="dnf update -y"
         installs="dnf install -y"
-        apps=("python3.11" "vim-common")
+        check_install="dnf list installed"
+        apps=("python3.11" "python3.11-cryptography" "vim-common")
     elif [[ "$release" == "centos" || "$release" == "oracle" ]]; then
         updates="yum update -y"
         installs="yum install -y"
-        apps=("python3.11" "vim-common")
+        check_install="yum list installed"
+        apps=("python3.11" "python3.11-cryptography" "vim-common")
     elif [[ "$release" == "alpine" ]]; then
         updates="apk update -f"
         installs="apk add -f"
-        apps=("python3" "xxd")
+        check_install="apk info -e"
+        apps=("python3" "py3-cryptography" "xxd")
     fi
 }
 
 install_base(){
     check_pmc
-    cmds=("python3" "xxd")
     echo -e "${Info} 你的系统是${Red} $release $os_version ${Nc}"
-    for g in "${!cmds[@]}"; do
-        if [ ! $(type -p ${cmds[g]}) ]; then
-            CMDS+=(${cmds[g]})
-            DEPS+=(${apps[g]})
+    echo
+    msg_deps=()
+    for g in "${!apps[@]}"; do
+        if ! $check_install "${apps[$g]}" &> /dev/null; then
+            msg_deps+=("${apps[$g]}")
         fi
     done
-
-    if [ "${#DEPS[@]}" -ge 1 ]; then
-        echo -e "${Info} 安装依赖列表：${Green}${CMDS[@]}${Nc}"
-        $updates >/dev/null 2>&1
-        $installs ${DEPS[@]} >/dev/null 2>&1
+    
+    if [ ${#msg_deps[@]} -gt 0 ]; then
+        echo -e "${Tip} 安装依赖列表：${Green}${msg_deps[@]}${Nc}"
+        $updates &> /dev/null
+        $installs "${msg_deps[@]}" &> /dev/null
     else
         echo -e "${Info} 所有依赖已存在，不需要额外安装。"
+    fi
+
+    if [[ "$release" == "almalinux" || "$release" == "fedora" || "$release" == "rocky" || "$release" == "centos" || "$release" == "oracle" ]]; then
+        ln -sf /usr/bin/python3.11 /usr/bin/python3
     fi
 }
 
